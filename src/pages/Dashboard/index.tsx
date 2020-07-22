713cca0641ba4b44c80059d945d634faa46ad492
William Moreschi <williammoreschi@gmail.com>
2020-07-22 00:19:57 -0300

    Criado o Header da aplicação

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+import { Container, Headar, HeaderContent, Profile } from './styles';
+import { useAuth } from '../../hooks/AuthContext';
 
-const Dashboard: React.FC = () => (
-  <Container>
-    <h1>Dashboard</h1>
-  </Container>
-);
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+  return (
+    <Container>
+      <Headar>
+        <HeaderContent>
+          <img src={logo} alt="Gobarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Seja Bem vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Headar>
+    </Container>
+  );
+};
 
 export default Dashboard;
