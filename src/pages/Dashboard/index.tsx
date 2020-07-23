9410f2e0cee604590445628d766f494eca8c3b38
William Moreschi <williammoreschi@gmail.com>
2020-07-22 22:25:46 -0300

    Criado a estrutura  da listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
-import { Container, Headar, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Headar,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 import { useAuth } from '../../hooks/AuthContext';
 
 const Dashboard: React.FC = () => {
@@ -24,6 +35,77 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Headar>
+
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>dia 06</span>
+            <span>Seguda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/2512512?v=4"
+                alt="William Moreschi"
+              />
+              <strong>William Moreschi</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/2512512?v=4"
+                  alt="William Moreschi"
+                />
+                <strong>William Moreschi</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                11:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/2512512?v=4"
+                  alt="William Moreschi"
+                />
+                <strong>William Moreschi</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                16:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/2512512?v=4"
+                  alt="William Moreschi"
+                />
+                <strong>William Moreschi</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -52,3 +52,151 @@ export const Profile = styled.div`
     }
   }
 `;
+
+export const Content = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+export const Schedule = styled.section`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+  p {
+    margin-top: 8px;
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+  }
+  span {
+    display: flex;
+    align-items: center;
+  }
+
+  span + span::before {
+    content: '';
+    width: 1px;
+    height: 12px;
+    background: #ff9000;
+    margin: 0 8px;
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    font-size: 20px;
+    font-weight: 400;
+    color: #999591;
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+
+    &::before {
+      content: '';
+      width: 1px;
+      height: 80%;
+      background: #ff9000;
+      position: absolute;
+      left: 0px;
+      top: 10%;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #ffffff;
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+
+      svg {
+        color: #ff9000;
+        margin-right: 8px;
+      }
+    }
+  }
+`;
+
+export const Section = styled.section`
+  margin-top: 48px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    line-height: 26px;
+    border-bottom: 1px solid #3e3b47;
+    display: block;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+`;
+
+export const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > div {
+    margin-bottom: 16px;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    color: #f4ede8;
+
+    svg {
+      color: #ff9000;
+      margin-right: 8px;
+    }
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-left: 24px;
+    flex: 1;
+
+    img {
+      width: 56px;
+      height: 56px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #ffffff;
+      font-size: 20px;
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
+`;
